5e100263dba1cdc9b7f1799dd8456d560196e01b
JorgeMateos13 <mateosjorge026@gmail.com>
2023-11-12 18:58:57 -0600

    RoleController

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\RolesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(RolesController::class)->group(function () {
+    Route::post('nuevoRol', 'addRole');
+    Route::get('roles', 'getRoles');
+    Route::get('role/{id}', 'getRoleByIde');
+    Route::put('editarRole/{id}', 'updateRole');
+    Route::delete('deleteRole/{id}', 'deleteRole');
 });
